9cd378c3507fecc02f09eb4c5779230247b9d086
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-01-10 14:03:03 +0800

    Update install script

--- install.sh
+++ install.sh
@@ -1,25 +1,50 @@
 #!/usr/bin/env bash
+SYMLINK=
+
+while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
+  -h | --help)
+    echo "Usage: $0 [--symlink]"
+    exit
+    ;;
+  -s | --symlink | --link ) SYMLINK=1 ;;
+esac; shift; done
 
 for fn in \
   ${HOME}/.mozilla/firefox/*.default \
   ${HOME}/Library/Application\ Support/Firefox/Profiles/*.default \
   ${HOME}/Library/Mozilla/Firefox/Profiles/*.default ; do
-  if [[ "$fn" =~ "*" ]]; then continue; fi
+  # shellcheck disable=SC2076
+  if [[ "$fn" =~ "\*" ]]; then continue; fi
   mkdir -p "$fn/chrome"
 
   fullpath="$fn/chrome/userChrome.css"
-  if [[ -e "$fullpath" ]]; then
-    echo ""
-    echo " !  Failed to install the Firefox theme."
-    echo "    $fullpath exists; you may need to delete it first."
-    echo ""
-    exit 1
+
+  echo ""
+  echo "$fullpath:"
+
+  # Add -f/--force to delete it before writing
+  if [[ -n "$FORCE" ]]; then
+    rm -f "$fullpath"
+  fi
+
+  if [[ -e "$fullpath" ]] || [[ -h "$fullpath" ]]; then
+    echo -ne " ! This file exists. Replace it? [Yn] "
+    read choice
+    if [[ "$choice" != "n" ]]; then
+      rm -f "$fullpath"
+    else
+      exit 1
+    fi
+  fi
+
+  if [[ -n "$SYMLINK" ]]; then
+    ln -nfs userChrome.css "$fullpath"
+    echo "   Symlinked. Restart Firefox to apply!"
   else
     cp userChrome.css "$fullpath"
-    echo ""
-    echo "  Installed to '$fullpath'."
-    echo "  Restart Firefox!"
-    echo ""
+    echo "   Copied. Restart Firefox to apply!"
   fi
+
+  echo ""
 done
 
