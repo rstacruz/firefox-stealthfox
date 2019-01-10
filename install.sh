0cf74752277944551a4d0ff870516716ff19b45d
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-01-10 13:53:34 +0800

    Fix install.sh script

--- install.sh
+++ install.sh
@@ -1,11 +1,25 @@
-#!usr/bin/env bash
+#!/usr/bin/env bash
+
 for fn in \
   ${HOME}/.mozilla/firefox/*.default \
   ${HOME}/Library/Application\ Support/Firefox/Profiles/*.default \
   ${HOME}/Library/Mozilla/Firefox/Profiles/*.default ; do
   if [[ "$fn" =~ "*" ]]; then continue; fi
   mkdir -p "$fn/chrome"
-  cp userChrome.css "$fn/chrome/userChrome.css"
-  echo "Installed to '$fn/chrome/userChrome.css'."
+
+  fullpath="$fn/chrome/userChrome.css"
+  if [[ -e "$fullpath" ]]; then
+    echo ""
+    echo " !  Failed to install the Firefox theme."
+    echo "    $fullpath exists; you may need to delete it first."
+    echo ""
+    exit 1
+  else
+    cp userChrome.css "$fullpath"
+    echo ""
+    echo "  Installed to '$fullpath'."
+    echo "  Restart Firefox!"
+    echo ""
+  fi
 done
 
